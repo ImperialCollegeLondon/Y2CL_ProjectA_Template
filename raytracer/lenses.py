ce2ca6b92196d656477634bbfd2ca522e09fd947
Alexander Richards <a.richards@imperial.ac.uk>
2023-07-20 16:17:40 +0100

    Removing advanced code.

--- raytracer/lenses.py
+++
@@ -1,7 +0,0 @@
-class ConvexPlano(OpticalElement):
-    def __init__(self, z_0, thickness, aperture, curv, n_out, n_in):
-        pass
-
-class BestLens(OpticalElement):
-    def __init__(self, z_0, thickness, curv1, curv2, n_in, n_out):
-        pass
