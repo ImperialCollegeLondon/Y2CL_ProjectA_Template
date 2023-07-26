247c5f20ef39f66500e101ab95c9dbf2b0a72491
Alexander Richards <a.richards@imperial.ac.uk>
2023-07-26 13:48:34 +0100

    Add better debugging and robustness to quality tests

--- tests/test_code_quality.py
+++ tests/test_code_quality.py
@@ -8,7 +8,7 @@ class TestStyle:
 
     def setup_class(self):
         project_path = os.path.split(os.path.dirname(__file__))[0]
-        course_file = glob(os.path.join(project_path, "raytracer", "*.py"))
+        course_file = glob(repr(path) for path in os.path.join(project_path, "raytracer", "[a-zA-Z]*.py"))
 
         pylint_results = Run(course_file, exit=False)
         self.style_result = pylint_results.linter.stats.global_note
@@ -32,9 +32,17 @@ class TestDocumentation:
 
     def test_documentation_present(self):
         project_path = os.path.split(os.path.dirname(__file__))[0]
-        course_files = " ".join(glob(os.path.join(project_path, "raytracer", "*.py")))
+        course_files = " ".join(repr(path) for path in glob(os.path.join(project_path, "raytracer", "[a-zA-Z]*.py")))
         cmd = rf"pydocstyle --select=D100,D102,D103,D419 {course_files}"
         res = run(cmd, shell=True, capture_output=True, check=False, text=True)
+        if res.returncode:
+            print("pydocstyle")
+            print("---------")
+            print(f"cmd:\n{cmd}")
+            print(f"return code: {res.returncode}")
+            print(f"stdout:\n{res.stdout}")
+            print(f"stderr:\n{res.stderr}")
+        assert res.returncode == 0, "pydocstyle failed"
         missing_docs = len(res.stdout.splitlines()) / 2
         if missing_docs:
             print(res.stdout)
@@ -42,11 +50,18 @@ class TestDocumentation:
 
     def test_documentation_style(self):
         project_path = os.path.split(os.path.dirname(__file__))[0]
-        course_files = " ".join(glob(os.path.join(project_path, "raytracer", "*.py")))
+        course_files = " ".join(repr(path) for path in glob(os.path.join(project_path, "raytracer", "[a-zA-Z]*.py")))
         cmd = rf"darglint {course_files}"
         res = run(cmd, shell=True, capture_output=True, check=False, text=True)
-        print(res.stdout)
-        malformed_docs = len(res.stdout.splitlines()) - 1
+        if res.returncode:
+            print("darglight")
+            print("---------")
+            print(f"cmd:\n{cmd}")
+            print(f"return code: {res.returncode}")
+            print(f"stdout:\n{res.stdout}")
+            print(f"stderr:\n{res.stderr}")
+        assert res.returncode == 0, "darglint failed"
+        malformed_docs = len(res.stdout.splitlines())
         if malformed_docs:
             print(malformed_docs)
         assert malformed_docs == 0
