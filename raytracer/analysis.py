f165cb61bf1f0e59ca22587cd0bffbf863f97714
Alexander Richards <inferno_alex@hotmail.com>
2024-01-23 15:40:31 +0000

    updates

--- raytracer/analysis.py
+++ raytracer/analysis.py
@@ -1,32 +1,149 @@
+"""Analysis module."""
+import matplotlib.pyplot as plt
 
-def task7():
-    # no returns
-    pass
 
-def task9():
-    # return fig
-    pass
+def task8():
+    """
+    task 8.
+
+    In this function you should check your propagate_ray function properly
+    finds the correct intercept and correctly refracts a ray. Don't forget
+    to check that the correct values are appended to your Ray object.
+    """
+
 
 def task10():
-    # return fig, focal_length
-    pass
+    """
+    task 10.
+
+    In this function you should create Ray objects with the given initial positions.
+    These rays should be propagated through the surface, up to the output plane.
+    You should then plot the tracks of these rays.
+    This function should return the matplotlib figure of the ray paths.
+
+    Returns:
+        Figure: the ray path plot.
+    """
+
 
 def task11():
-    # return fig
-    pass
+    """
+    task 11.
+
+    In this function you should propagate the three given paraxial rays through the system
+    to the output plane and the tracks of these rays should then be plotted.
+    This function should return the following items as a tuple in the following order:
+    1. the matplotlib figure object for ray paths
+    2. the calculated focal point.
+
+    Returns:
+        tuple[Figure, float]: the ray path plot and the focal point
+    """
+
 
 def task12():
-    # return fig, rms_value
-    pass
+    """
+    task 12.
+
+    In this function you should create a RayBunble and propagate it to the output plane
+    before plotting the tracks of the rays.
+    This function should return the matplotlib figure of the track plot.
+
+    Returns:
+        Figure: the track plot.
+    """
+
 
 def task13():
-    # return fig
-    pass
+    """
+    task 13.
+
+    In this function you should again create and propagate a RayBundle to the output plane
+    before plotting the spot plot.
+    This function should return the following items as a tuple in the following order:
+    1. the matplotlib figure object for the spot plot
+    2. the simulation RMS
+
+    Returns:
+        tuple[Figure, float]: the spot plot and rms
+    """
+
 
 def task14():
-    # return plano_convex fig, plano_convex rms_val, convex_plano fig, convex_plano rms_val
-    pass
+    """
+    Task14.
+
+    In this function you will trace a number of RayBundles through the optical system and
+    plot the RMS and diffraction scale dependence on input beam radii.
+    This function should return the following items as a tuple in the following order:
+    1. the matplotlib figure object for the diffraction scale plot
+    2. the simulation RMS for input beam radius 2.5
+    3. the diffraction scale for input beam radius 2.5
+
+    Returns:
+        tuple[Figure, float, float]: the plot, the simulation RMS value, the diffraction scale.
+    """
+
 
 def task15():
-    # return fig
-    pass
+    """
+    Task 15.
+
+    In this function you will create plano-convex lenses in each orientation and propagate a RayBundle
+    through each to their respective focal point. You should then plot the spot plot for each orientation.
+    This function should return the following items as a tuple in the following order:
+    1. the matplotlib figure object for the spot plot for the plano-convex system
+    2. the focal point for the plano-convex lens
+    3. the matplotlib figure object for the spot plot for the convex-plano system
+    4  the focal point for the convex-plano lens
+
+
+    Returns:
+        tuple[Figure, float, Figure, float]: the spot plots and rms for plano-convex and convex-plano.
+    """
+
+
+def task16():
+    """
+    Task16.
+
+    In this function you will be again plotting the radial dependence of the RMS and diffraction values
+    for each orientation of your lens.
+    This function should return the following items as a tuple in the following order:
+    1. the matplotlib figure object for the diffraction scale plot
+    2. the RMS for input beam radius 3.5 for the plano-convex system
+    3. the RMS for input beam radius 3.5 for the convex-plano system
+    4  the diffraction scale for input beam radius 3.5
+
+    Returns:
+        tuple[Figure, float, float, float]: the plot, RMS for plano-convex, RMS for convex-plano, diffraction scale.
+    """
+
+
+if __name__ == "__main__":
+
+    # Run task 8 function
+    task8()
+
+    # # Run task 10 function
+    # fig10 = task10()
+
+    # # Run task 11 function
+    # fig11, focal_point = task11()
+
+    # # Run task 12 function
+    # fig12 = task12()
+
+    # # Run task 13 function
+    # fig13, rms = task13()
+
+    # # Run task 14 function
+    # fig14, rms, diff_scale = task14()
+
+    # # Run task 15 function
+    # fig15_pc, focal_point_pc, fig15_cp, focal_point_cp = task15()
+
+    # # Run task 16 function
+    # fig16, pc_rms, cp_rms, diff_scale = task16()
+
+    plt.show()
